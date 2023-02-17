e04e93fcf0689be2719ce6c0908d02b98d10970d
Rafay <111191103+refrain-n@users.noreply.github.com>
2023-02-17 20:42:41 +0500

    bookmark feature

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
+//runs when popup is loaded
 document.addEventListener('DOMContentLoaded', function () {
+
+    //initialize a bookmark if it already doesnt exist
+    if (chrome.storage.local.get('bookmarkExists', function (result) {
+        if (result.bookmarkExists) {
+            return false
+        }
+    })) {
+        chrome.bookmarks.create({
+            parentId: '1',
+            title: "ReVisit",
+            url: undefined
+        },
+            (bookmark) => {
+                console.log('Bookmark created with ID:', bookmark.id);
+                chrome.bookmarks.update(bookmark.id, { pinned: true }, function () {
+                    console.log('Bookmark pinned!');
+                    chrome.storage.local.set({ 'bookmarkExists': true, 'bookmarkId': bookmark.id });
+                });
+            });
+    }
+
+
     var inputField = document.getElementById('input-field');
     var submitBtn = document.getElementById('submit-btn');
     var text = document.getElementById('text');
@@ -21,7 +44,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 chrome.runtime.sendMessage({ current: current }, function (response) {
                     var result = response.result;
                     if (result) {
-                        chrome.tabs.create({url: result.url, active: true });
+                        chrome.tabs.create({ url: result.url, active: true });
                     } else {
                         console.log('No results found');
                     }
@@ -29,7 +52,7 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     }
-    
+
 
     chrome.storage.local.get('web', function (result) {
         var current = result.web;
@@ -43,9 +66,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
     visitBtn.addEventListener('click', visitCurrentWebsite);
 
-    chrome.runtime.onSuspend.addListener(function() {
+    chrome.runtime.onSuspend.addListener(function () {
         // Remove event listeners
         submitBtn.removeEventListener('click', saveCurrentWebsite);
         visitBtn.removeEventListener('click', visitCurrentWebsite);
     });
-});
+
+
+
+
+
+});  
--- scripts/background.js
+++ scripts/background.js
@@ -1,7 +1,9 @@
+
+//Runs when a new message is received from index.j
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     if (message.current) {
         var current = message.current;
-        console.log('Current value:', current);
+        console.log('Currently value :', current);
         // do something with the current value
 
         chrome.history.search(
@@ -9,20 +11,35 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
                 maxResults: 3,
                 text: current
             },
-            (results)=>{
+            (results) => {
                 if (results.length > 0) {
                     // get the first result
                     var result = results[0];
-                    console.log('Most recent visit:', result.url);
+                    console.log('Most recent visit:', result.url , typeof result.url);
                     // do something with the result
                     sendResponse({ result: result });
                 } else {
                     console.log('No results found');
                 }
+
+                chrome.storage.local.get('bookmarkId', function (result) {
+                    const bookmarkId = result.bookmarkId
+                    chrome.bookmarks.update(bookmarkId,
+                    { url: result.url},
+                    ()=>{
+                        console.log('Bookmark updated!');
+                    }
+                )
+
+                });
+                
+            
             }
         );
-    }
 
+        
+    }
     // Return true to indicate that the response will be sent asynchronously
     return true;
 });
+
